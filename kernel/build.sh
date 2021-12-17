8526128292a6edf95c67891fdc614e59344a0303
Nan Wang <wangnangg@gmail.com>
2021-12-17 13:05:21 -0800

    fix kernel dependency on linker script

--- kernel/build.sh
+++ kernel/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 set -x
-cargo build --release
+rm -f target/aarch64/release/kernel*
+rm -f target/aarch64/release/deps/kernel-*
+cargo build --release -vvv
 llvm-objcopy -O binary target/aarch64/release/kernel target/aarch64/release/kernel.img
--- load_kernel.sh
+++ load_kernel.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-pushd ../kernel
+pushd ./kernel
 ./build.sh
 popd
 
+pushd ./uart_host
 cargo run -- ../kernel/target/aarch64/release/kernel.img
+popd
 
 
